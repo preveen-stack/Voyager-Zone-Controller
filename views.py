74240852c6be592a2704324261cc064433117e4a
vishnu3983 <srivishnusudhakar@gmail.com>
2018-10-11 15:54:41 +0530

    dump added and file split

--- views.py
+++ views.py
@@ -1,2 +1,17 @@
-from app import app
+from app import app, main
 from models import *
+from flask import jsonify
+
+@app.route('/commissioning', methods = ['GET'])
+def commissioningMethod():
+    static_data = []
+    xbee_devices = []
+    objects = StaticData.objects()
+    for i in range(StaticData.objects.count()):
+        static_data.append(StaticData.objects.get(id=objects[i].id))
+    xbee_objects = XbeeDevices.objects()
+    for i in range(XbeeDevices.objects.count()):
+        xbee_devices.append(XbeeDevices.objects.get(id=xbee_objects[i].id))
+    return jsonify({"staticData": static_data, "XbeeDevice": xbee_devices})
+
+
