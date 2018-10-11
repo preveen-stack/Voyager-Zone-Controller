49736bbc38745857f692c7810a848908aba19c8d
vishnu3983 <srivishnusudhakar@gmail.com>
2018-10-11 15:54:41 +0530

    dump added and file split

--- app.py
+++ app.py
@@ -11,7 +11,11 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+from flask import Flask
+from flask_mqtt import Mqtt
 
+from pymongo import MongoClient
+import time
 from digi.xbee.devices import XBeeDevice
 import re
 from flask_mongoengine import MongoEngine
@@ -24,24 +28,18 @@ PORT = "/dev/ttyUSB0"
 BAUD_RATE = 9600
 
 
-from flask import Flask
-from flask_mqtt import Mqtt
-
-from pymongo import MongoClient
-import time
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
 
-toSend = ''
+
+toSend=''
 mqtt = Mqtt(app)
 #mqtt.subscribe('voyager')
 
 db = MongoEngine(app)
 client = MongoClient('localhost', 27017)
 mongo = client.voyagerDB
-
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
@@ -66,11 +64,7 @@ def handle_logging(client, userdata, level, buf):
 
 device = XBeeDevice(PORT, BAUD_RATE)
 
-class SimpleUpdate(db.Document):
-    angle = db.FloatField()
-    pvVoltage = db.FloatField()
-    batteryVoltage = db.FloatField()
-
+from models import XbeeDevices, SimpleUpdate
 def main():
     print(" +-------------------------------------------------+")
     print(" | 			ZC 			      |")
@@ -89,29 +83,60 @@ def main():
                  device.send_data_broadcast(toSend)
                  toSend = ''
 
+            if discoverFlagSet:
+                xbee_network = device.get_network()
+
+                xbee_network.set_discovery_timeout(15)  # 15 seconds.
+
+                xbee_network.clear()
+
+                # Callback for discovered devices. TODO save device ID to db in seperate collection
+                def callback_device_discovered(remote):
+                    xbee_device = XbeeDevices(deviceID=device_id)              #TODO how to get device_id?
+                    xbee_device.save()
+                    print("Device discovered: %s" % remote)
+
+                # Callback for discovery finished.
+                def callback_discovery_finished(status):
+                    if status == NetworkDiscoveryStatus.SUCCESS:
+                        print("Discovery process finished successfully.")
+                    else:
+                        print("There was an error discovering devices: %s" % status.description)
+
+                xbee_network.add_device_discovered_callback(callback_device_discovered)
+
+                xbee_network.add_discovery_process_finished_callback(callback_discovery_finished)
+
+                xbee_network.start_discovery_process()
+
+                print("Discovering remote XBee devices...")
+
+                while xbee_network.is_discovery_running():
+                    time.sleep(0.1)
+
             xbee_message = device.read_data()
             if xbee_message is not None:
-                 print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(), xbee_message.data.decode()))
+                 print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),xbee_message.data.decode()))
                  payload=xbee_message.data.decode()
                  if(payload == "REQUEST_INIT_DATA"):
                     device.send_data_broadcast(INIT_DATA)
                     print("init data broadcasted")
                  elif 'angle' in payload:
-                    arr = re.findall(r"[-+]?\d*\.\d+|\d+",payload)
+                    arr=re.findall(r"[-+]?\d*\.\d+|\d+",payload)
                     updateData = SimpleUpdate(angle=float(arr[0]),pvVoltage=float(arr[1]),batteryVoltage=float(arr[2]))
                     updateData.save()
                     mqtt.publish('voyager/site001',payload)
                     print("message published")
-                 else:
+                 else :
                     print(payload)
     finally:
         if device is not None and device.is_open():
             device.close()
 
 
-from views import *
-
 
 if __name__ == '__main__':
     main()
     app.run(host='0.0.0.0', port=5000)
+
+
--- models.py
+++ models.py
@@ -25,6 +25,8 @@ class Misc(db.EmbeddedDocument):
     windSpeed = db.FloatField()
     ambientTemp = db.FloatField()
     boardTemp = db.FloatField()
+    timeToStow = db.FloatField()
+
 
 class Led(db.EmbeddedDocument):
     power = db.StringField()
@@ -37,14 +39,64 @@ class Event(db.EmbeddedDocument):
     time = db.FloatField()
     desc = db.StringField()
 
+class Stow(db.EmbeddedDocument):
+    snowStow = db.FloatField()
+    windStow = db.FloatField()
+    nightStow = db.FloatField()
+    cleanStow = db.FloatField()
+
+class Limits(db.EmbeddedDocument):
+    east = db.FloatField()
+    west = db.FloatField()
+
+class Position(db.EmbeddedDocument):
+    lat = db.FloatField()
+    lng = db.FloatField()
+    alt = db.FloatField()
+    azimuthDeviation = db.FloatField()
+    pitch_east = db.FloatField()
+    pitch_west = db.FloatField()
+    rowWidth = db.FloatField()
+
+class Motor(db.EmbeddedDocument):
+    trackingResolution = db.FloatField()
+
+class ControllerInfo(db.EmbeddedDocument):
+    siteName = db.StringField()
+    siteID = db.StringField()
+    zoneID = db.StringField()
+    firmwareVersion = db.StringField()
+    macID = db.StringField()
+    boardSerialNo = db.StringField()
+    threshold_wind_speed = db.FloatField()
+    table_length = db.FloatField()
+    table_width = db.FloatField()
+    stow = db.EmbeddedDocumentField(Stow)
+    limits = db.EmbeddedDocumentField(Limits)
+    position = db.EmbeddedDocumentField(Postion)
 
 class UpdateData(db.Document):
-    motorUpdate = db.EmbeddedDocumentField(MotorUpdate)
+    trackerID = db.StringField()
+    timeStamp = db.FloatField()
+    motor = db.EmbeddedDocumentField(MotorUpdate)
     battery = db.EmbeddedDocumentField(Battery)
     pv = db.EmbeddedDocumentField(PV)
-    tracking = db.EmbeddedDocumentField(PV)
-    misc = db.EmbeddedDocumentField(PV)
-    led = db.EmbeddedDocumentField(PV)
+    tracking = db.EmbeddedDocumentField(Tracking)
+    misc = db.EmbeddedDocumentField(Misc)
+    led = db.EmbeddedDocumentField(Led)
     events = db.ListField(db.EmbeddedDocumentField(Event))
+
+class SimpleUpdate(db.Document):
+    angle = db.FloatField()
+    pvVoltage = db.FloatField()
+    batteryVoltage = db.FloatField()
+
+class StaticData(db.Document):
+    trackerID = db.StringField()
+    controllerInfo = db.EmbeddedDocumentField(ControllerInfo)
+    motor = db.EmbeddedDocumentField(Motor)
+
+class XbeeDevices(db.Document):
+    deviceID = db.StringField()                 #TODO will XbeeDevices collection contain only deviceID?
 
 
